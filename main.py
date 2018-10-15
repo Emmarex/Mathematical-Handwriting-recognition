c1763b6b291975789ad61e657fdd49f8f164ac57
Emmarex <tairuoluwafemi09@gmail.com>
2018-10-15 13:14:43 +0100

    added latex transformation

--- main.py
+++ main.py
@@ -79,6 +79,24 @@ def train_model():
     else :
         print("An error occurred.")
 
+def transform_to_latex(handwritten_text):
+    latex_symbols_dict = {
+        '-' : '-',',' : ',','!' : '!','(' : '(',')' : ')',
+        '[' : '[',']' : ']','{' : '{','}' : '}','+' : '+','=' : '=',
+        '0' : '0','1' : '1','2' : '2','3' : '3','4' : '4','5' : '5',
+        '6' : '6','7' : '7','8' : '8','9' : '9','A' : 'A','alpha' : '\\alpha',
+        'ascii_124' : '|','b' : 'b','beta' : '\\beta','C' : 'C','cos' : '\\cos','d' : 'd',
+        'Delta' : '\\delta','div' : '\\div','e' : 'e','exists' : '\\exists','f' : 'f','\\forall' : 'forall',
+        'forward_slash' : '/','G' : 'G','gamma' : '\\gamma','geq' : '\\geq','gt' : '>','H' : 'H',
+        'i' : 'i','in' : '\\in','infty' : '\\infty','int' : '\\int','j' : 'j','k' : 'k',
+        'l' : 'l','lambda' : '\\lambda','ldots' : '\\ldots','leq' : '\leq','\\lim' : 'lim','log' : '\log',
+        'lt':'<','M':'M','mu':'\mu','N':'N','neq':'\\neq','o':'o','p':'p','phi':'\\phi','pi':'\\pi',
+        'pm':'\\pm','prime':'\\prime','q':'q','R':'R','rightarrow':'\Rightarrow','S':'S','sigma':'\\sigma',
+        'sin':'\\sin','sqrt':'\\sqrt','sum':'\sum','T':'T','tan':'\\tan','theta':'\\theta','times':'\\times',
+        'u':'u','v':'v','w':'w','X':'X','y':'y','z':'z'
+    }
+    return latex_symbols_dict.get(handwritten_text)
+
 def main():
     try:
         args = sys.argv
@@ -89,7 +107,7 @@ def main():
             try:
                 saved_decision_tree_classifier_model = pickle.load(open("Model/decision_tree_classifier.pkl",'rb'))
                 model_prediction = saved_decision_tree_classifier_model.predict(image_file)
-                print(f"Recognized Digit : {model_prediction[0]}")
+                print(f"Recognized Digit : {model_prediction[0]} \n Latex Equivalent : {transform_to_latex(model_prediction[0])}")
             except FileNotFoundError as model_file_error:
                 print(f"Error : {model_file_error}")
                 print("... Training Model")
